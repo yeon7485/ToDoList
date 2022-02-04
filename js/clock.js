841ca9ba7e43fea8f27eafcf96d0fca17bf6286e
yeon7485 <wsx4379@naver.com>
2022-02-04 21:55:04 +0900

    css finish

--- js/clock.js
+++ js/clock.js
@@ -4,7 +4,7 @@ function getClock() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
-    clock.innerText = `${hours} : ${minutes}`;
+    clock.innerText = `${hours}:${minutes}`;
 }
 
 getClock();
